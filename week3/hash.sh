76ad7c2866b9a3bf0e10ade70c0adef92d2ba8c5
hayesbm3 <hayesbm3@miamioh.edu>
2021-02-09 17:38:24 -0500

    week3

---
+++ week3/hash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -X POST -d "{\"key\":\"${1}\",\"password\":\"test\",\"uid\":\"campbest\"}" \
+                -H "Content-Type: application/json" \
+                        http://ceclnx01.cec.miamioh.edu/~campbest/cse451-campbest-web-public-2021/kv/kv.php/api/v1/hash
